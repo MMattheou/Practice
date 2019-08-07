5d766a1b54183599582d56e065d495e3eba29008
MMattheou <manthos.matt@hotmail.com>
2019-08-07 17:56:19 +0300

    interview practice

--- src/Main.kt
+++ src/Main.kt
@@ -1,37 +1,92 @@
+enum class ColourOfCard {
+    BLACK,
+    RED
+}
+
+enum class TypeOfCard {
+    HEARTS,
+    SPADES,
+    DIAMONDS,
+    CLUBS
+}
+
+data class Cards (val typeOfCard: TypeOfCard, val colourOfCard: ColourOfCard)
+
+
+
+
+
+
 fun main(args: Array<String>) {
-    var questOne = "cat"
-    var resultOne = ""
-    questOne?.forEach {
-        resultOne = it + resultOne
-    }
-    if (questOne?.toUpperCase() == resultOne.toUpperCase()) {
-        println("$questOne is a palindrome")
-    } else {
-        println("$questOne is not a palindrome")
-    }
 
-    var num = 1001
-    var resultTwo = ""
 
-    num?.toString().forEach {
-        resultTwo = it + resultTwo
-    }
-    if (num?.toString() == resultTwo) {
-        println("$num is a palindrome")
-    } else {
-        println("$num is not a palindrome")
-    }
+    val deckOfCards = mutableListOf<Cards>()
+    val playerHand = mutableListOf<Cards>()
 
-    for (i in 1..30) {
-        var isPrime = true
-        for (j in 2 until i) {
-            if (i % j == 0) {
-                isPrime = false
-            }
-        }
-        if (isPrime == true) {
-            println("$i is a prime")
-        }
-    }
+    deckOfCards.add(Cards(TypeOfCard.HEARTS, ColourOfCard.RED))
+    deckOfCards.add(Cards(TypeOfCard.HEARTS, ColourOfCard.BLACK))
+    deckOfCards.add(Cards(TypeOfCard.DIAMONDS, ColourOfCard.RED))
+    deckOfCards.add(Cards(TypeOfCard.DIAMONDS, ColourOfCard.BLACK))
+    deckOfCards.add(Cards(TypeOfCard.SPADES, ColourOfCard.RED))
+    deckOfCards.add(Cards(TypeOfCard.SPADES, ColourOfCard.BLACK))
+    deckOfCards.add(Cards(TypeOfCard.CLUBS, ColourOfCard.RED))
+    deckOfCards.add(Cards(TypeOfCard.CLUBS, ColourOfCard.BLACK))
+
+
+
+//   String var questOne = "cat"
+//    var resultOne = ""
+//    questOne?.forEach {
+//        resultOne = it + resultOne
+//    }
+//    if (questOne?.toUpperCase() == resultOne.toUpperCase()) {
+//        println("$questOne is a palindrome")
+//    } else {
+//        println("$questOne is not a palindrome")
+//    }
+//
+//    var num = 1001
+//    var resultTwo = ""
+//
+//    num?.toString().forEach {
+//        resultTwo = it + resultTwo
+//    }
+//    if (num?.toString() == resultTwo) {
+//        println("$num is a palindrome")
+//    } else {
+//        println("$num is not a palindrome")
+//    }
+//
+//    for (i in 1..30) {
+//        var isPrime = true
+//        for (j in 2 until i) {
+//            if (i % j == 0) {
+//                isPrime = false
+//            }
+//        }
+//        if (isPrime == true) {
+//            println("$i is a prime")
+//        }
+//    }
+//    exercise("Am I am and I am not")
+
+println(draw(deckOfCards))
+}
+
+//fun exercise(sent: String) {
+////    val temp =  sent.split(" ").groupingBy { it.toLowerCase() }.eachCount()
+////    println(temp)
+//    sent.split(" ").forEach {
+//       var counter = 0
+//        for (i in sent.split(" ")) {
+//            if (it.toLowerCase() == i.toLowerCase()) {
+//                counter++
+//            }
+//        }
+//        println("The word \"$it\" is found $counter times")
+//    }
+//}
+fun draw (lista : List<Cards>) : Cards{
+    return lista.random()
 }
 
